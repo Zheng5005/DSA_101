2d3861a1db26f2fdc64dc51165dbad5cb4d7e2e1
Zheng5005 <u.gomezherrera@gmail.com>
2025-07-02 14:16:33 -0600

    First LinkedList exercise

--- LinkedList/linkedList.go
+++ LinkedList/linkedList.go
@@ -41,5 +41,45 @@ func main()  {
 	myList.Add(10)
 	myList.Add(20)
 	myList.Add(30)
+	myList.Add(40)
+	myList.Add(50)
 	myList.Print()
+
+	myList.Exercise1_rotate(4)
+	fmt.Println("Rotated left by 4: ")
+	myList.Print()
+}
+
+func (l *LinkedList) Exercise1_rotate(k int)  {
+	// Given a singly linked list and an integer k, the task is to rotate the linked list to the left by k places.
+	if l.Head == nil || l.Head.Next == nil || k == 0 {
+		return
+	}
+
+	// Counting total nodes
+	current := l.Head
+	count := 1
+	for current.Next != nil {
+		current = current.Next
+		count++
+	}
+
+	// If k is greater than or equal to list length, use modulo
+	k = k % count
+	if k == 0 {
+		return
+	}
+
+	// Now current points to the last node
+	current.Next = l.Head // this makes the LinkedList circular
+
+	// Traverse to (k)th node
+	temp := l.Head
+	for i := 1; i < k; i++ {
+		temp = temp.Next
+	}
+
+	// Setting new head and break the circle
+	l.Head = temp.Next
+	temp.Next = nil
 }
